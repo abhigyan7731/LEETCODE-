e2e83554e4025dfaa7cf78594547a680778cd3a6
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2026-06-25 12:55:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.3%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left =0, right = nums.size() - 1, curr = 0;
+
+        while (curr <= right){
+            if (nums[curr] == 0){
+                swap(nums[left], nums[curr]);
+                left++;
+                curr++;
+            
+        }
+        else if (nums[curr] == 2) {
+            swap(nums[right], nums[curr]);
+            right--;
+        }else {
+            curr++;
+        }
+        }
+    }
+};
