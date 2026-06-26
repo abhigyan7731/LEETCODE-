70d8afce140ffb14263556c8302054e15dea3383
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2026-06-26 20:51:59 +0530

    Time: 19 ms (78.15%), Space: 95.6 MB (80.79%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,53 @@
+class BIT {
+public:
+    int n;
+    vector<int> bit;
+
+    BIT(int sz) {
+        n = sz;
+        bit.assign(n + 1, 0);
+    }
+
+    void update(int idx, int val) {
+        while (idx <= n) {
+            bit[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        while (idx > 0) {
+            sum += bit[idx];
+            idx -= idx & -idx;
+        }
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        BIT ft(2 * n + 1);
+
+        // Shift prefix sums to positive indices
+        int pref = n + 1;
+        ft.update(pref, 1);
+
+        long long ans = 0;
+
+        for (int x : nums) {
+            if (x == target)
+                pref++;
+            else
+                pref--;
+
+            ans += ft.query(pref - 1);
+            ft.update(pref, 1);
+        }
+
+        return ans;
+    }
+};
