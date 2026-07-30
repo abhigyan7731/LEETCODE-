f907e1872e4e90a28246f782c9db4f8130e927ed
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2026-07-30 09:14:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int ans = 0;
+        int k = 1;
+
+        for (int i =0; i < n/8; i++){
+            ans += k * 8;
+            k++;
+        }
+        ans += k * (n % 8);
+        return ans;
+
+        
+    }
+};
