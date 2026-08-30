84171bd6318ba93a87c68031d0d8e5fc505787c8
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2026-08-30 15:40:32 +0530

    Time: 13 ms (36.15%), Space: 26 MB (84.54%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        vector<int> mask(n);
+
+        for (int i=0; i<n; i++){
+            for (char ch : words[i]){
+                mask[i] |= (1 << (ch - 'a'));
+            }
+        }
+        int ans = 0;
+
+        for (int i=0; i <n; i++){
+            for (int j = i+1; j < n; j++){
+                if (( mask[i] & mask[j]) == 0)
+                {
+                    ans = max(ans,(int) (words[i].size() * words[j].size()));
+            }
+        }
+        
+    }
+    return ans;
+    }
+};
